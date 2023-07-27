a86c51dd034fd73f939efe5c0dfc5b098bf57289
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-07-27 20:43:24 +0500

    add 5 migration

---
+++ app/models/add_defolt_value_table_test.rb
@@ -0,0 +1,2 @@
+class AddDefoltValueTableTest < ApplicationRecord
+end
---
+++ test/models/add_defolt_value_table_test_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AddDefoltValueTableTestTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
