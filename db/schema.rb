aba4674bd3fc9d5ff69e6373dc3fae6388635815
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-07-29 19:54:53 +0500

    add_seeds, Test.desc_category, User.my_tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_27_130238) do
+ActiveRecord::Schema.define(version: 2023_07_29_054950) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2023_07_27_130238) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "passed_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_passed_tests_on_test_id"
+    t.index ["user_id"], name: "index_passed_tests_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Test.create([
+  {title: 'Ruby', level: 2, category_id: 1 },
+  {title: 'Rails', level: 3, category_id: 2 },
+  {title: 'SQL', level: 2, category_id: 3 }])
+
+Answer.create([
+  {body: 'Lerom', correct: true, question_id: 1 },
+  {body: 'Dor', correct: false, question_id: 1 },
+  {body: 'Col', correct: false, question_id: 1 },
+  {body: 'Mol', correct: false, question_id: 1 } ])
+
+Category.create([
+  { title: 'Hyk' },
+  { title: 'Look' },
+  { title: 'Fix' }])
+
+Question.create([
+  {body: 'Who?', test_id: 1 },
+  {body: 'Author of the Ruby language?', test_id: 1 },
+  {body: 'PS?', test_id: 3 }])
+
+User.create([
+  {name: 'Dima', email:'dima.@mail.ru' },
+  {name: 'Alina', email: 'alina.@mail.ru' },
+  {name: 'Katia', email: 'katia.@mail.ru' },
+  {name: 'Boris', email: 'boris.@mail.ru' } ])
+  
