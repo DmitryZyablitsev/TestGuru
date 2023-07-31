daef7b51d78ece707aa7869982b8c48827409692
DmitryZyablitsev <dima.zyablicev@mail.ru>
2023-07-31 15:18:07 +0500

    fix desc_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :users, through: :passed_tests
 
   def self.desc_category(category_name)
-    cat_id = Category.find_by("title = ?", category_name).id
+    cat_id = Category.where(titile: catregory_name).id.id
     Test.where(category_id: cat_id).order(title: :desc)
   end
 end
